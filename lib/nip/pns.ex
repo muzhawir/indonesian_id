25f6f684652a0a3cb9fd27a61e7875659c283d7b
muzhawir <muzhawir.amri@gmail.com>
2025-01-22 18:48:35 +0800

    Refactoring Nip modules

--- lib/nip/pns.ex
+++ lib/nip/pns.ex
@@ -3,7 +3,6 @@ defmodule Nip.Pns do
   Functions for working with NIP PNS (Nomor Induk Pegawai Negeri Sipil)
 
   NIP PNS consists of 18 digits divided into 4 parts:
-  NIP PNS consists of 18 digits that divided into 4 parts:
   - `8` digits for birth date (`YYYYMMDD`)
   - `6` digits for TMT (Tanggal Mulai Tugas) (`YYYYMM`)
   - `1` digit for sex code (`1` for male and `2` for female)
@@ -13,14 +12,15 @@ defmodule Nip.Pns do
   import Nip.Utils
 
   @type nip_result() :: {:ok, t()} | {:error, String.t()}
-  @type t :: %__MODULE__{
+  @type t() :: %__MODULE__{
           nip: String.t(),
           birth_date: Date.t(),
           tmt_date: Date.t(),
           sex: String.t(),
           serial_number: String.t()
         }
-  @type date_result() :: {:ok, Date.t()} | {:error, String.t()}
+  @type validated_nip_result() :: {:ok, String.t()} | {:error, String.t()}
+  @typep date_result() :: {:ok, Date.t()} | {:error, atom()}
 
   defstruct [:nip, :birth_date, :tmt_date, :sex, :serial_number]
 
@@ -79,7 +79,7 @@ defmodule Nip.Pns do
       {:ok, "200012312024121001"}
 
   """
-  @spec validate_format(String.t()) :: {:ok, String.t()} | {:error, String.t()}
+  @spec validate_format(String.t()) :: validated_nip_result()
   def validate_format(nip) when is_binary(nip) do
     with {:ok, _} <- validate_length(nip),
          {:ok, _} <- birth_date(nip),
