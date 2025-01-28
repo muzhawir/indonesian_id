5d8f02132b6f6d0affd4a685704d0224df57b93a
Muzhawir Amri <muzhawir.amri@gmail.com>
2025-01-28 18:55:36 +0800

    #11 Refactor Regional Find Functions to One Argument (#12)
    - Refactor regional find functions to use a single argument.
    - Fix Nik.Utils functions for finding regional data.
    - Remove the Guard module.

--- lib/regional/utils.ex
+++ lib/regional/utils.ex
@@ -7,10 +7,11 @@ defmodule Regional.Utils do
   @doc """
   Searches regional data based on a series of code structures.
 
+  Returns `{:ok, data}` if the data is found; otherwise, returns an error with a specific message.
   Returns `{:ok, data}` if the data is found; otherwise, returns `{:error, "Data not found"}`.
   """
   @spec search_code(regional_data(), atom()) :: regional_result()
-  def search_code(nil, _), do: {:error, "Data not found"}
+  def search_code(nil, type), do: {:error, error_message(type)}
   def search_code(data, type), do: {:ok, extract_data(data, type)}
 
   defp extract_data(data, type) do
@@ -21,4 +22,9 @@ defmodule Regional.Utils do
       :subdistrict -> Map.take(data, ["code", "type", "name"])
     end
   end
+
+  defp error_message(:province), do: "Province data not found"
+  defp error_message(:city), do: "City data not found"
+  defp error_message(:district), do: "District data not found"
+  defp error_message(:subdistrict), do: "Subdistrict data not found"
 end
--- test/regional_test.exs
+++ test/regional_test.exs
@@ -5,45 +5,61 @@ defmodule RegionalTest do
 
   describe "find_province/1" do
     test "returns expected result when data is present" do
+      input = %{province_code: "72"}
       expected_result = {:ok, %{"code" => "72", "name" => "Sulawesi Tengah"}}
-      assert Regional.find_province("72") == expected_result
+
+      assert Regional.find_province(input) == expected_result
     end
 
     test "returns error when data is not present" do
-      assert Regional.find_province("2025") == {:error, "Data not found"}
+      input = %{province_code: "2025"}
+
+      assert Regional.find_province(input) == {:error, "Province data not found"}
     end
   end
 
   describe "find_city/2" do
     test "returns expected result when data is present" do
+      input = %{province_code: "72", city_code: "10"}
       expected_result = {:ok, %{"code" => "10", "name" => "Sigi", "type" => "Kabupaten"}}
-      assert Regional.find_city("72", "10") == expected_result
+
+      assert Regional.find_city(input) == expected_result
     end
 
     test "returns error when data is not present" do
-      assert Regional.find_province("2025") == {:error, "Data not found"}
+      input = %{province_code: "72", city_code: "1000"}
+
+      assert Regional.find_city(input) == {:error, "City data not found"}
     end
   end
 
   describe "find_district/3" do
     test "returns expected result when data is present" do
+      input = %{province_code: "72", city_code: "10", district_code: "14"}
       expected_result = {:ok, %{"code" => "14", "name" => "Marawola", "type" => "Kecamatan"}}
-      assert Regional.find_district("72", "10", "14") == expected_result
+
+      assert Regional.find_district(input) == expected_result
     end
 
     test "returns error when data is not present" do
-      assert Regional.find_province("2025") == {:error, "Data not found"}
+      input = %{province_code: "72", city_code: "10", district_code: "1400"}
+
+      assert Regional.find_district(input) == {:error, "District data not found"}
     end
   end
 
   describe "find_subdistrict/4" do
     test "returns expected result when data is present" do
+      input = %{province_code: "72", city_code: "10", district_code: "14", subdistrict_code: "2007"}
       expected_result = {:ok, %{"code" => "2007", "name" => "Tinggede", "type" => "Desa"}}
-      assert Regional.find_subdistrict("72", "10", "14", "2007") == expected_result
+
+      assert Regional.find_subdistrict(input) == expected_result
     end
 
     test "returns error when data is not present" do
-      assert Regional.find_province("2025") == {:error, "Data not found"}
+      input = %{province_code: "72", city_code: "10", district_code: "14", subdistrict_code: "20070"}
+
+      assert Regional.find_subdistrict(input) == {:error, "Subdistrict data not found"}
     end
   end
 end
